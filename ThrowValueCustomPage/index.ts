08857f4487fb4fba214e948ddaf06bc6f08d9221
Marc Fernández García <m.fernandez@axazure.com>
2023-11-22 18:19:11 +0100

    SetUp ThrowValueCustomPage

---
+++ ThrowValueCustomPage/index.ts
@@ -0,0 +1,29 @@
+import {IInputs, IOutputs} from "./generated/ManifestTypes";
+
+export class ThrowValueCustomPage implements ComponentFramework.StandardControl<IInputs, IOutputs> {
+    private context_: ComponentFramework.Context<IInputs>
+	constructor() {
+
+	}
+    
+	public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container: HTMLDivElement): void {
+		this.context_ = context;
+		sessionStorage.removeItem(this.context_.parameters.sessionStorageKey.raw!);
+	}
+    
+	public updateView(context: ComponentFramework.Context<IInputs>): void {
+        debugger;
+		let props = this.context_.parameters.data.raw;
+		if (props.result == true) {
+			sessionStorage.setItem(this.context_.parameters.sessionStorageKey.raw!, JSON.stringify(props));
+		}
+	}
+    
+	public getOutputs(): IOutputs {
+		return {};
+	}
+
+	public destroy(): void {
+		
+	}
+}
